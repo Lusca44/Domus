64f91b3a7a3019c94ef935ab2924f40a022d06f1
Lucas Santos <devlucas44@gmail.com>
2025-05-14 22:58:19 -0300

    Inicio projeto

---
+++ src/test/java/br/com/domus/domus/DomusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domus.domus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
