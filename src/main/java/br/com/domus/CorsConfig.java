d874c9a73e2c377a154435f506bae443e3bdc477
Lucas Santos <devlucas44@gmail.com>
2025-07-08 23:20:44 -0300

    alterando cors para aceitar dns do site vercel

--- src/main/java/br/com/domus/CorsConfig.java
+++ src/main/java/br/com/domus/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         
         // Permitir localhost:8081 (seu frontend)
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("domuns-front-lucas-santos-projects-d53d8b43.vercel.app");
+        config.addAllowedOrigin("domuns-front.vercel.app");
         
         // Permitir métodos necessários
         config.addAllowedMethod("GET");
