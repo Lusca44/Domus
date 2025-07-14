fafe02f9c87188e01ffa0985fd82da5a3a3c64d8
Lucas Santos <devlucas44@gmail.com>
2025-07-14 11:30:42 -0300

    tentativa envio de email

---
+++ src/main/java/br/com/domus/aplicacao/emailsender/dto/ImovelAnunciosSiteDTO.java
@@ -0,0 +1,21 @@
+package br.com.domus.aplicacao.emailsender.dto;
+
+public record ImovelAnunciosSiteDTO(String nome, //
+		String telefone, //
+		String email, //
+		String finalidade, //
+		String tipo, //
+		String endereco, //
+		String bairro, //
+		String cidade, //
+		String cep, //
+		String quartos, //
+		String banheiros, //
+		String vagas, //
+		String area, //
+		String valor, //
+		String descricao, //
+		String observacoes//
+) {
+
+}
